4c5d12a19519651f320380e6cec9c340feee29ad
ititifad <dex90jay@gmail.com>
2020-04-12 17:55:12 +0300

    changed database

--- src/codex/codex/settings.py
+++ src/codex/codex/settings.py
@@ -79,14 +79,10 @@ WSGI_APPLICATION = 'codex.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'School',
-        'USER': 'postgres',
-        'HOST': 'localhost',
-        'PASSWORD': 'Pavilion@@'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
