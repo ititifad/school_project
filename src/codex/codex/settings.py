eaab599e1edbf2209c2840b1511f8074615d3c5c
ititifad <dex90jay@gmail.com>
2020-04-13 23:35:45 +0300

    modified Procfile, settings.py and requirements.txt

--- src/codex/codex/settings.py
+++ src/codex/codex/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -137,3 +138,5 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'dex90jay@gmail.com'
 EMAIL_HOST_PASSWORD = 'mhcgmcvufskcrzjw'
+
+django_heroku.settings(locals())
